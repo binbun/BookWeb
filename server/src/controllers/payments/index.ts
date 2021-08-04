0bb96855ecb0775e8d425fe174917a815cd3bbf7
binbun <hoangbinbun@gmail.com>
2021-08-04 21:58:26 +0700

    aa
    aa

---
+++ server/src/controllers/payments/index.ts
@@ -0,0 +1,56 @@
+const Payments = require('../models/paymentModel')
+const Users = require('../models/userModel')
+const Products = require('../models/productModel')
+import { Request, Response, NextFunction } from "express"
+import { IProduct } from "../../types/todo"
+import { IGetUserAuthInfoRequest } from "../users"
+
+type cartItem = {
+    _id: string;
+    quantity: number;
+    sold: number;
+}
+
+const paymentCtrl = {
+    getPayments: async(req: Request, res: Response) =>{
+        try {
+            const payments = await Payments.find()
+            res.json(payments)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    createPayment: async(req: IGetUserAuthInfoRequest, res: Response) => {
+        try {
+            const user = await Users.findById(req.user.id).select('name email')
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            const {cart, paymentID, address} = req.body;
+
+            const {_id, name, email} = user;
+
+            const newPayment = new Payments({
+                user_id: _id, name, email, cart, paymentID, address
+            })
+
+            cart.filter((item: cartItem) => {
+                return sold(item._id, item.quantity, item.sold)
+            })
+
+            
+            await newPayment.save()
+            res.json({msg: "Payment Succes!"})
+            
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
+}
+
+const sold = async (id: string, quantity: number, oldSold: number) =>{
+    await Products.findOneAndUpdate({_id: id}, {
+        sold: quantity + oldSold
+    })
+}
+
+module.exports = paymentCtrl
---
+++ server/src/routes/payments.ts
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const paymentCtrl = require('../controllers/paymentCtrl')
+import auth from '../middleware/auth'
+import authAdmin from '../middleware/authAdmin'
+
+
+router.route('/payment')
+    .get(auth, authAdmin, paymentCtrl.getPayments)
+    .post(auth, paymentCtrl.createPayment)
+
+
+module.exports = router
